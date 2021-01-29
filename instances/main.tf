2d5c82ae75fe27e4ce4a5c2d20239a8b3da27b20
Rachel Sharp <rachel@hashicorp.com>
2021-01-28 22:39:35 -0500

    update providers

--- instances/main.tf
+++ instances/main.tf
@@ -1,5 +1,16 @@
+terraform {
+  required_version = "~> 0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
 provider "aws" {
-  region = var.region
+  region = var.aws_region
 }
 
 data "aws_ami" "ubuntu" {
